77e1a7fe1b3508dc4b3e2d6ebd284f9f759baad3
shoaibswe <shoaibswe@gmail.com>
2023-03-23 14:04:49 +0600

    updated load and save acc to str convert pass

--- excel_storage.py
+++ excel_storage.py
@@ -7,10 +7,10 @@ class ExcelStorage:
 
     def load_accounts(self):
         if os.path.exists(self.filename):
-            df = pd.read_excel(self.filename, index_col=0)
+            df = pd.read_excel(self.filename, index_col=0, dtype={'password': str})  # Ensure password is read as a string
             accounts = df.to_dict(orient='index')
             for username in accounts:
-                accounts[username]['balance'] = int(accounts[username]['balance'])  # Convert balance to integer
+                accounts[username]['balance'] = int(accounts[username]['balance'])
             return accounts
         else:
             return {}
@@ -19,7 +19,7 @@ class ExcelStorage:
         df = pd.DataFrame.from_dict(accounts, orient='index', columns=['password', 'balance'])
         df.index.name = 'username'
         df = df.reset_index()
-        df['username'] = df['username'].astype(str)  # Ensure username is stored as a string
+        df['username'] = df['username'].astype(str)
+        df['password'] = df['password'].astype(str)  # Ensure password is stored as a string
         df = df.set_index('username')
-        df.to_excel(self.filename)
-
+        df.to_excel(self.filename, index=True)
